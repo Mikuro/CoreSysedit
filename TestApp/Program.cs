eaee0f0db6518ad76b960d81b129991fa4572bbc
Anton Pushpyshev <an.pushpyshev@gmail.com>
2022-06-17 06:15:46 +0300

    Add files via upload

---
+++ TestApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using tanks.Models;
+using tanks.Models.Solver;
+
+using System.Xml;
+using System.Xml.Linq;
+
+namespace tanks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FlowDiagram fd1;
+
+            using (var fs = new FileStream(args[0], FileMode.Open))
+                fd1 = XMLUtil.DeserializeFlowDiagram(fs);
+
+            fd1.Fix();
+
+
+            tanks.TestCode.SolverTest(fd1);
+        }
+    }
+}
---
+++ TestData/TestData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace tanks
+{
+    public static class TestData
+    {
+        public static UnmanagedMemoryStream GetResourceStream(string resName)
+        {
+            var assembly = typeof(TestData).Assembly;
+            var stream = assembly.GetManifestResourceStream(resName);
+/*
+            var strResources = assembly.GetName().Name + ".g.resources";
+            var rStream = assembly.GetManifestResourceStream(strResources);
+            var resourceReader = new System.Resources.ResourceReader(rStream);
+            var items = resourceReader.OfType<System.Collections.DictionaryEntry>();
+            var stream = items.First(x => (x.Key as string) == resName.ToLower()).Value;
+*/
+            return (UnmanagedMemoryStream)stream;
+        }
+    }
+}
